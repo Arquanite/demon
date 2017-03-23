c4b9145ff321ca772d187a6711d01d84968a82d9
dzieszk <dzieszk@gmail.com>
2017-03-23 11:30:27 +0100

    Bieganie

--- main.c
+++ main.c
@@ -49,6 +49,11 @@ int main(int argc, int *argv[]){
 	syslog(LOG_INFO, "Start programu");
 	widelec();
 	printf("Wyglada na to, ze dziala\n");
+	while(1){
+		syslog(LOG_INFO, "Biegam sobie xd");
+		sleep(5);
+		syslog(LOG_INFO, "Dalej se biegam");
+	}
 
 	closelog();
 	return 0;
