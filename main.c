ec5e2d2011d3ac3a902f768fc02334fb236d3b8c
dzieszk <dzieszk@gmail.com>
2017-03-23 11:15:24 +0100

    Zmiana katalogu

--- main.c
+++ main.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <syslog.h>
-#include <unistd.h>
+#include <stdlib.h> // EXIT_SUCCESS i EXIT_FAILURE
+//#include <sys/types.h>
+#include <sys/stat.h> // umask
+#include <syslog.h>  // syslogów
+#include <unistd.h> // fork
 
 //funkcja forkująca rodzica
 void widelec(){
 	syslog(LOG_INFO, "Użycie widelca");
+	
 	pid_t pid, sid;
 	pid = fork();
-	if(pid < 0) {
+	if(pid < 0){
+		syslog(LOG_ERR, "Error: could not fork parent process");
 		exit(EXIT_FAILURE);
 	}
 	if(pid > 0){
 		exit(EXIT_SUCCESS);
 	}
 	umask(0);
+
+	sid = setsid();
+	if(sid < 0){
+		syslog(LOG_ERR, "Error: could not create sid for child process");
+		exit(EXIT_FAILURE);
+	}
+
+	if((chdir("/")) < 0) {
+		syslog(LOG_ERR, "Error: could not change working directory");
+	}
 }
 
 int main(int argc, int *argv[]){
