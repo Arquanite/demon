178235c397dc905b6486f75744545cf8f5ebfce5
dzieszk <dzieszk@gmail.com>
2017-03-23 09:10:30 +0100

    Dodany widelec

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, int argv*[]){
+widelec(){
+    pid_t pid;
+    pid = fork();
+    if(pid < 0) {
+        exit(EXIT_FAILURE)
+    }
+    if(pid > 0){
+        exit(EXIT_SUCCESS);
+    }
+}
+
+int main(int argc, int *argv[]){
     printf("Wyglada na to, ze dziala");
     return 0;
 }
