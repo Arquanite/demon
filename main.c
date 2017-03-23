848c2edecac978773abd4cadf9a332191825140c
Arquanite <krynkaxd@gmail.com>
2017-03-23 11:01:37 +0100

    spaces to tabs

--- main.c
+++ main.c
@@ -7,28 +7,28 @@
 
 //funkcja forkująca rodzica
 void widelec(){
-    syslog(LOG_INFO, "Użycie widelca");
-    pid_t pid, sid;
-    pid = fork();
-    if(pid < 0) {
-        exit(EXIT_FAILURE);
-    }
-    if(pid > 0){
-        exit(EXIT_SUCCESS);
-    }
-    umask(0);
+	syslog(LOG_INFO, "Użycie widelca");
+	pid_t pid, sid;
+	pid = fork();
+	if(pid < 0) {
+		exit(EXIT_FAILURE);
+	}
+	if(pid > 0){
+		exit(EXIT_SUCCESS);
+	}
+	umask(0);
 }
 
 int main(int argc, int *argv[]){
-    if(argc < 3){
-	printf("Jesteś debilem debilu :u\n");
-	return 1;
-    }
-    openlog("demon_log", LOG_PID|LOG_CONS, LOG_USER);
-    syslog(LOG_INFO, "Start programu");
-    widelec();
-    printf("Wyglada na to, ze dziala\n");
+	if(argc < 3){
+		printf("Jesteś debilem debilu :u\n");
+		return 1;
+	}
+	openlog("demon_log", LOG_PID|LOG_CONS, LOG_USER);
+	syslog(LOG_INFO, "Start programu");
+	widelec();
+	printf("Wyglada na to, ze dziala\n");
 
-    closelog();
-    return 0;
+	closelog();
+	return 0;
 }
