6c9ed1121122910cd4192ad4193a81020f99d310
dzieszk <dzieszk@gmail.com>
2017-03-23 10:49:46 +0100

    Baza dla logów

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <syslog.h>
 #include <unistd.h>
 
 //funkcja forkująca rodzica
 void widelec(){
+    syslog(LOG_INFO, "Użycie widelca");
     pid_t pid, sid;
     pid = fork();
     if(pid < 0) {
@@ -18,6 +20,11 @@ void widelec(){
 }
 
 int main(int argc, int *argv[]){
+    openlog("demon_log", LOG_PID|LOG_CONS, LOG_USER);
+    syslog(LOG_INFO, "Start programu");
+    widelec();
     printf("Wyglada na to, ze dziala\n");
+
+    closelog();
     return 0;
 }
