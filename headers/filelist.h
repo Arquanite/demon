4a60eb270a0f9c3feac615fcf0d25e6594a6bae5
Arquanite <krynkaxd@gmail.com>
2017-04-03 21:29:28 +0200

    LISTA!!11

--- headers/filelist.h
+++ headers/filelist.h
@@ -1,10 +1,21 @@
 #ifndef FILELIST_H
 #define FILELIST_H
 
+#include <stdlib.h> // NULL
+#include "bool.h"
+
 /* Lista, służy do przechowywania listy plików w katalogu */
 
 typedef struct {
-    int lol;
+    char *name; // nazwa pliku/katalogu
+    bool mmap; // czy plik powinien być kopiowany za pomocą mmap
+    struct file_list *next; // wskaźnik na następny element
 } file_list;
+
+/* Dodaje nowy element do listy, zwraca adres ostatniego elementu */
+file_list* add(file_list *list, char *name, bool mmap);
+
+/* Usuwa wszystkie elementy listy */
+void remove_all(file_list *first);
 
 #endif
--- sources/filelist.c
+++ sources/filelist.c
@@ -1,1 +1,21 @@
 #include "filelist.h"
+
+file_list *add(file_list *list, char *name, bool mmap){
+    while(list->next != NULL) list = (file_list*)list->next;
+    list->next = malloc(sizeof(file_list));
+    list = (file_list*)list->next;
+    list->name = name;
+    list->mmap = mmap;
+    list->next = NULL;
+    return list;
+}
+
+void remove_all(file_list *first){
+    if(first == NULL) return;
+    while(first->next != NULL){
+        file_list *prev = first;
+        first = (file_list*)first->next;
+        free(prev);
+    }
+    free(first);
+}
