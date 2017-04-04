68ad0a9f505836ea2ccc5747a562afbcd55670be
Arquanite <krynkaxd@gmail.com>
2017-04-04 10:02:45 +0200

    usunięto (file_list*) bo już nie trzeba

--- sources/filelist.c
+++ sources/filelist.c
@@ -7,9 +7,9 @@ file_list *list_create(){
 }
 
 file_list *list_add(file_list *list, char *name, char *path, bool mmap){
-    while(list->next != NULL) list = (file_list*)list->next;
+    while(list->next != NULL) list = list->next;
     list->next = malloc(sizeof(file_list));
-    list = (file_list*)list->next;
+    list = list->next;
     list->name = name;
     list->path = path;
     list->mmap = mmap;
@@ -21,7 +21,7 @@ void list_remove_all(file_list *first){
     if(first == NULL) return;
     while(first->next != NULL){
         file_list *prev = first;
-        first = (file_list*)first->next;
+        first = first->next;
         free(prev);
     }
     free(first);
